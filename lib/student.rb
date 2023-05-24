c60fb79297c7e9789f87cc83192a3b54caaac0e5
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-24 11:06:30 -0400

    Add student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
+
 class Student
- 
+    attr_reader :name, :age, :scores
+    def initialize(student_info)
+      @name = student_info[:name]
+      @age = student_info[:age]
+      @scores = []
+    end
+    def log_scores(score)
+      @scores << score
+    end
+    def grade
+      @scores.sum.fdiv(@scores.count)
+    end
+  end
 end
