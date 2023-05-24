e796656ef1a056739475858f4129e83d332b5842
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-24 11:31:30 -0400

    Add full? and enroll methods to course class

--- lib/course.rb
+++ lib/course.rb
@@ -1,10 +1,22 @@
 class Course
-    attr_reader :name, :capacity, :students
+attr_reader :name, :capacity, :students
 
     def initialize(name, capacity)
         @name = name
         @capacity = capacity
         @students = []
     end
-
+        
+    def full? 
+        if @students.count >= capacity
+            true
+        else 
+            false
+        end
+    end
+        
+    def enroll(student)
+        @students<<student
+    end
 end
+
