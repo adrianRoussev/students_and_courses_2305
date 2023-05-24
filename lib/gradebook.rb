66087135aa582c1e5c0ef872debc7c180c8b2f50
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-24 12:12:14 -0400

    Add  and edit gradebook class edit gradebook tests

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,60 @@
-class Gradebook
-  
-end
+class GradeBook
+    attr_reader :instructor, :courses
+    def initialize(instructor)
+        @instructor = instructor
+        @courses = []
+    end
+    
+    def add_course(course)
+        @courses << course
+    end
+    
+    def list_all_students
+        all_students = {}
+        courses.each do |course|
+            student_names = []
+            course.students.each do |student|
+                student_names << student.name
+                all_students.update({course.name => student_names})
+            end
+        end
+        all_students
+    end
+    
+    def students_below(threshold)
+        student_names_below = []
+        courses.each do |course|
+            course.students.each do |student|
+                if student.grade < threshold
+                student_names_below << student.name
+            end
+        end
+    end
+    student_names_below
+    end
+    
+    def list_all_grades
+        all_grades = {}
+        courses.each do |course|
+            student_grades = []
+            course.students.each do |student|
+                student_grades << student.grade
+                all_grades.update({course.name => student_grades})
+            end
+        end
+        all_grades
+    end
+    
+    def students_in_range(min,max)
+        student_names_in_range = []
+        courses.each do |course|
+            course.students.each do |student|
+                if student.grade <= max && student.grade  >= min
+                    student_names_in_range << student.name
+                end
+            end
+        end
+        student_names_in_range
+    end
+
+end    
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -4,34 +4,34 @@ require './lib/gradebook'
 require './lib/course'
 require './lib/student'
 
-RSpec.describe Gradebook do 
+RSpec.describe GradeBook do 
     it 'exists' do
         gradebook=GradeBook.new("adrian")
-        expect(gradebook).to be_an_instance_of(Gradebook)
-      end
+        expect(gradebook).to be_an_instance_of(GradeBook)
+    end
     
-      it 'has an instructor' do
+    it 'has an instructor' do
         gradebook=GradeBook.new("adrian")
         expect(gradebook.instructor).to eq("adrian")
-      end
+    end
         
-     
-      it 'has an other instructor' do
-        gradebook=GradeBook.new("Mark")
-      expect(gradebook.instructor).to eq("Mark")
-      end
-
-   
     
-      it 'has courses' do
+    it 'has an other instructor' do
+        gradebook=GradeBook.new("Mark")
+    expect(gradebook.instructor).to eq("Mark")
+    end
+
+
+    
+    it 'has courses' do
         student1 = Student.new({name: "Morgan", age: 21})
         student2 = Student.new({name: "Jordan", age: 29}) 
         course1 = Course.new("Calculus", 2) 
         gradebook=GradeBook.new("adrian") 
         expect(gradebook.courses).to eq([])
-      end
+    end
 
-      it 'can add courses' do
+    it 'can add courses' do
         student1 = Student.new({name: "Morgan", age: 21})
         student2 = Student.new({name: "Jordan", age: 29}) 
         course1 = Course.new("Calculus", 2) 
@@ -40,9 +40,9 @@ RSpec.describe Gradebook do
         gradebook=GradeBook.new("adrian") 
         gradebook.add_course(course1)
         expect(gradebook.courses).to eq([course1])
-      end
+    end
 
-      it 'can list all students' do
+    it 'can list all students' do
         student1 = Student.new({name: "Morgan", age: 21})
         student2 = Student.new({name: "Jordan", age: 29}) 
         course1 = Course.new("Calculus", 2) 
@@ -51,10 +51,10 @@ RSpec.describe Gradebook do
         gradebook=GradeBook.new("adrian") 
         gradebook.add_course(course1)
         expect(gradebook.list_all_students).to eq({"Calculus"=>["Morgan", "Jordan"]})
-      end
+    end
 
-      
-      it 'can list all grades' do
+    
+    it 'can list all grades' do
         student1 = Student.new({name: "Morgan", age: 21})
         student2 = Student.new({name: "Jordan", age: 29}) 
         student1.log_scores(70)
@@ -65,18 +65,18 @@ RSpec.describe Gradebook do
         gradebook=GradeBook.new("adrian") 
         gradebook.add_course(course1)
         expect(gradebook.list_all_grades).to eq("Calculus"=>[70.0, 90.0])
-end
+    end
 
-it 'can find students in range' do
-    student1 = Student.new({name: "Morgan", age: 21})
-    student2 = Student.new({name: "Jordan", age: 29}) 
-    student1.log_scores(70)
-    student2.log_scores(90)
-    course1 = Course.new("Calculus", 2) 
-    course1.enroll(student1) 
-    course1.enroll(student2) 
-    gradebook=GradeBook.new("adrian") 
-    gradebook.add_course(course1)
-    expect(gradebook.students_in_range(60,70)).to eq(["Morgan"])
-end
+    it 'can find students in range' do
+        student1 = Student.new({name: "Morgan", age: 21})
+        student2 = Student.new({name: "Jordan", age: 29}) 
+        student1.log_scores(70)
+        student2.log_scores(90)
+        course1 = Course.new("Calculus", 2) 
+        course1.enroll(student1) 
+        course1.enroll(student2) 
+        gradebook=GradeBook.new("adrian") 
+        gradebook.add_course(course1)
+        expect(gradebook.students_in_range(60,70)).to eq(["Morgan"])
+    end
 end
